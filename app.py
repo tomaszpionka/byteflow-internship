dc44492a49662dde7dd7966584c6106d5d101d35
DbartekK <debartek19@hotmail.com>
2024-04-10 15:56:36 +0200

    On branch bdyk-dev Your branch is up to date with 'origin/bdyk-dev'. 	new file:   main.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from time import sleep
+from datetime import datetime
+from main import str_concat_func
+
+
+print(datetime.now())
+sleep(2)
+print(datetime.now())
+sleep(2)
+print(datetime.now())
+str_concat_func(1, 2)
---
+++ pandas/data/pandas_demo.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('pandas/data/survey_results_public.csv')
+df.shape
