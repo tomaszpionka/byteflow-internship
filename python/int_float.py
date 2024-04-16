fc71373bf2c44a3a104f8bdbcab96210b04da867
DbartekK <debartek19@hotmail.com>
2024-04-16 22:14:07 +0200

    Numeric data operations

---
+++ python/int_float.py
@@ -0,0 +1,21 @@
+# Floor division
+print(3 // 2)
+
+# Exponent
+print(3 ** 2)
+
+# Operations order
+print(3 * (2 + 1))
+
+# Incrementation
+num = 1
+num += 1
+print(num)
+
+# Absolute value
+print(abs(-3))
+print(round(3.75,1))
+
+num_1 = int('100')
+num_2 = int('200')
+print(num_1 + num_2)
