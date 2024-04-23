0d40ddfb4fd3613dfb7cb0d228fd64e2d42b2f8b
DbartekK <debartek19@hotmail.com>
2024-04-23 13:39:24 +0200

    changed directory

--- python/python_exceptions.py
+++
@@ -1,9 +0,0 @@
-try:
-    f = open('not_found.txt')
-except FileNotFoundError:
-    print('Sorry. This file does not exist')
-
-try:
-    var = bad_var
-except NameError:
-    print('Variable not found')
